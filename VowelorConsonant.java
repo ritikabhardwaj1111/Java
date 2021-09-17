7bebf66caf0edcad9fd810410ea8f8cdd92759d1
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2021-09-18 00:26:45 +0530

    Update VowelorConsonant.java

--- VowelorConsonant.java
+++ VowelorConsonant.java
@@ -2,19 +2,19 @@
 
 import java.util.Scanner;
 class VowelorConsonant{
-public static void main(String args[])
-{
-System.out.println("Enter a Alphabet");
-Scanner sc = new Scanner(System.in);
-char val0 = sc.next().charAt(0);
-char val = Character.toLowerCase(val0);  //Converts input char into lowercase
-if(val=='a'|| val=='e'|| val=='i' || val=='o' || val=='u')
-{ 
-System.out.println(val+" is vowel");
-}
-else
-{
-System.out.println(val+" is Consonant");
-}
-}
+    public static void main(String args[])
+    {
+          System.out.println("Enter a Alphabet");
+          Scanner sc = new Scanner(System.in);
+          char val0 = sc.next().charAt(0);
+          char val = Character.toLowerCase(val0);          //Converts input char into lowercase
+            if(val=='a'|| val=='e'|| val=='i' || val=='o' || val=='u')
+                { 
+                    System.out.println(val+" is vowel");
+                }
+                  else
+                {
+                    System.out.println(val+" is Consonant");
+                }
+    }
 }
