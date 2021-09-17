655618279dc60a075314ac76695ab35b44610c79
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2021-09-17 15:29:10 +0530

    Update SwapNumber.java

--- SwapNumber.java
+++ SwapNumber.java
@@ -2,21 +2,20 @@
 
 import java.util.Scanner;
 class SwapNumber{
-    public static void main(String Args[])
-{
-System.out.println("Enter First number: ");
-Scanner sc = new Scanner(System.in);
-int num1 = sc.nextInt();
-System.out.println("Enter the second number: \n");
-Scanner sc0 = new Scanner(System.in);
-int num2 = sc0.nextInt();
-System.out.println("First number: "+num1);
-System.out.println("Second number: \n"+num2);
-int temp = num1;
-num1 = num2;
-num2 = temp;
-System.out.println("After Swapping numbers");
-System.out.println("First number: "+num1);
-System.out.println("Second number: "+num2);
+    public static void main(String Args[]){
+           System.out.println("Enter First number: ");
+           Scanner sc = new Scanner(System.in);
+           int num1 = sc.nextInt();
+           System.out.println("Enter the second number: \n");
+           Scanner sc0 = new Scanner(System.in);
+           int num2 = sc0.nextInt();
+           System.out.println("First number: "+num1);
+           System.out.println("Second number: \n"+num2);
+           int temp = num1;
+           num1 = num2;
+           num2 = temp;
+           System.out.println("After Swapping numbers");
+           System.out.println("First number: "+num1);
+           System.out.println("Second number: "+num2);
 }
 }
