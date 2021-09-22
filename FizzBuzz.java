4a06f6b936bf4e9ec388a775a9dc409e919267ef
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2021-09-22 17:30:43 +0530

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,28 @@
+/*  Java program which iterates the integers from 1 to 100.
+    For multiples of three print "Fizz" instead of the number and print "Buzz" for the multiples of five.
+    When number is divided by both three and five, print "fizz buzz". */
+import java.util.Scanner;
+class FizzBuzz{
+	public static void main(String args[]){
+			
+			for(int i=1; i<=100;i++)
+				{
+					if(i%15==0)
+						{
+						   System.out.println("Fizz Buzz");
+						}
+				        else if(i%5==0)
+						{
+						   System.out.println("Buzz");
+						   	
+						}
+				        else if(i%3==0)
+						{
+						   System.out.println("Fizz");
+						}
+ 					else
+						System.out.println(i);
+				}
+
+	   }
+}
