a8572233f86528f230e2a326db21993bd0b37563
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2021-09-27 22:35:15 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class SumOfArray{
+public static void main(String args[])
+{
+   int i,sum=0;
+   System.out.println("Enter the size of an array: ");
+   Scanner sc = new Scanner(System.in);
+   int size = sc.nextInt();
+   int[] arr = new int[size];
+   System.out.println("Enter "+size+" elements in array: ");
+   for(i=0;i<size;i++)
+	{
+          arr[i] = sc.nextInt();
+         // System.out.println(arr[i]);
+         }
+   for(i=0;i<size;i++)
+	{
+          sum += arr[i];
+	}  
+  System.out.println("Sum of elements is "+sum);
+}
+}
