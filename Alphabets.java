fdcca711873394d3f0da4ad2045f1b40ba552a94
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2021-09-30 22:35:02 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,11 @@
+/*  Display Alphabets (A to Z) using loop */
+class Alphabets{
+public static void main(String args[])
+		{ 
+  			char i;
+  			for(i='a';i<='z';i++)
+				{
+ 					System.out.println(i);
+				}
+		}
+	}
