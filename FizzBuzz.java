5d88469620d77a11cce590390b8b917e91c9d407
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2021-10-01 17:29:42 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,9 @@
-/*  Java program which iterates the integers from 1 to 100.
+/* 
+    Java program which iterates the integers from 1 to 100.
     For multiples of three print "Fizz" instead of the number and print "Buzz" for the multiples of five.
-    When number is divided by both three and five, print "fizz buzz". */
+    When number is divided by both three and five, print "fizz buzz". 
+*/
+
 import java.util.Scanner;
 class FizzBuzz{
 	public static void main(String args[]){
