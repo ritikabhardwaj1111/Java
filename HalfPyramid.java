1dfdcd5dd6d1276d47d0a6f77fc7992b64d9c8bf
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2021-10-01 19:41:43 +0530

    Update HalfPyramid.java

--- HalfPyramid.java
+++ HalfPyramid.java
@@ -1,4 +1,5 @@
 /*
+
 Print pattern:
 *
 * *
@@ -8,21 +9,22 @@ Print pattern:
 * * * * * *
 * * * * * * *
 * * * * * * * *
+
 */
 
 import java.util.Scanner;
 class HalfPyramid{
   public static void main(String[] args) {
-    Scanner obj = new Scanner(System.in);
-    System.out.print("Enter the number of rows: ");
-    int r = obj.nextInt();
-    for(int i=0;i<r;i++)
-    {
-      for(int j=0;j<i;j++)
-      {
-         System.out.print("* ");
-      }
-        System.out.println("\n");
-    }
+            Scanner obj = new Scanner(System.in);
+            System.out.print("Enter the number of rows: ");
+            int r = obj.nextInt();
+            for(int i=0;i<r;i++)
+                {
+                  for(int j=0;j<i;j++)
+                    {
+                        System.out.print("* ");
+                    }
+            System.out.println("\n");
+                }
+        }
   }
-}
