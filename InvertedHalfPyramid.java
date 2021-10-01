f3cf8374562084fcb8f9a6d546f7ded5403e8320
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2021-10-01 19:40:03 +0530

    Update InvertedHalfPyramid.java

--- InvertedHalfPyramid.java
+++ InvertedHalfPyramid.java
@@ -14,16 +14,16 @@ Print pattern:
 import java.util.Scanner;
 class InvertedHalfPyramid{
   public static void main(String args[]){
-  System.out.println("Enter the number of rows: ");
-  Scanner obj = new Scanner(System.in);
-  int r = obj.nextInt();
-  for(int i=r;i>=1;--i)
-  {
-    for(int j=1;j<=i;++j)
-    {
-       System.out.print("* ");
-    }
-      System.out.println("\n");
+          System.out.println("Enter the number of rows: ");
+          Scanner obj = new Scanner(System.in);
+          int r = obj.nextInt();
+            for(int i=r;i>=1;--i)
+              {
+                for(int j=1;j<=i;++j)
+                  {
+                        System.out.print("* ");
+                  }
+            System.out.println("\n");
+             }
+      }
   }
-}
-}
