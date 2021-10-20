c142fbf4080efa3389376aace65e5f09fe0d619e
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2021-10-20 22:28:03 +0530

    Update ArrayAverage.java

--- ArrayAverage.java
+++ ArrayAverage.java
@@ -2,20 +2,19 @@
 
 import java.util.Scanner;
 class ArrayAverage{
-public static void main(String args[]){
-int i,avg=0,sum=0;
-Scanner obj = new Scanner(System.in);
-System.out.println("Enter the size of an array ");
-int n = obj.nextInt();
-int arr[] = new int[n];
-System.out.println("Enter "+n+" elements in the array");
-for(i=0;i<n;i++)
-{
-  arr[i] = obj.nextInt();
-  sum+=arr[i];	
-}
-avg = sum/n;
-System.out.println(" "+avg);
-}
-
+      public static void main(String args[]){
+                   int i,avg=0,sum=0;
+                   Scanner obj = new Scanner(System.in);
+                   System.out.println("Enter the size of an array ");
+                   int n = obj.nextInt();
+                   int arr[] = new int[n];
+                   System.out.println("Enter "+n+" elements in the array");
+                   for(i=0;i<n;i++)
+                          {
+                              arr[i] = obj.nextInt();
+                              sum+=arr[i];	
+                          }
+                   avg = sum/n;
+                   System.out.println(" "+avg);
+                }
 }
